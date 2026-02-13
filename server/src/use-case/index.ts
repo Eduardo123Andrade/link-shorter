9c648385e823a008b3738e31f117b907f0387fa6
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2026-02-13 20:35:41 -0300

    feat: add check database use case

--- server/src/use-case/index.ts
+++ server/src/use-case/index.ts
@@ -1,4 +1,5 @@
-export { createLink } from "./create-link.use-case";
-export { findLinkByShortLink } from "./find-link-by-short-link.use-case";
-export { listAllLinks } from "./list-all-links.use-case";
-export { deleteLink } from "./delete-link.use-case";
+export { CheckDatabaseUseCase } from './check-database.use-case';
+export { createLink } from './create-link.use-case';
+export { findLinkByShortLink } from './find-link-by-short-link.use-case';
+export { listAllLinks } from './list-all-links.use-case';
+export { deleteLink } from './delete-link.use-case';
