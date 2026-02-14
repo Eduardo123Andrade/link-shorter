68fcb91e17feb6aa2f6d631fc726a58cd43f2dc9
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2026-02-14 19:27:38 -0300

    feat(server): allow protocol-less URLs and handle duplicate short links

--- server/src/use-case/create-link.use-case.ts
+++ server/src/use-case/create-link.use-case.ts
@@ -1,5 +1,6 @@
 import { LinkShorterRepository } from '../repository';
 import { generateUuid } from '../utils';
+import { LinkAlreadyExistsError } from '../errors';
 
 interface CreateLinkInput {
   link: string;
@@ -17,11 +18,22 @@ interface CreateLinkOutput {
 export const createLink = async (
   input: CreateLinkInput
 ): Promise<CreateLinkOutput> => {
+  const existingLink = await LinkShorterRepository.findByShortLink(input.shortLink);
+
+  if (existingLink) {
+    throw new LinkAlreadyExistsError();
+  }
+
+  let finalLink = input.link;
+  if (!/^https?:\/\//i.test(finalLink)) {
+    finalLink = `http://${finalLink}`;
+  }
+
   const id = generateUuid();
 
   const savedLink = await LinkShorterRepository.save({
     id,
-    link: input.link,
+    link: finalLink,
     shortLink: input.shortLink,
   });
 
