b1a1e5795cef7ceaffffce756ee733bd1e838f0a
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2026-02-14 18:10:07 -0300

    fix: remove inital data

--- web/src/store/linkStore.ts
+++ web/src/store/linkStore.ts
@@ -8,20 +8,7 @@ interface LinkStore {
 }
 
 export const useLinkStore = create<LinkStore>((set) => ({
-  links: [
-    {
-      id: "1",
-      shortUrl: "Portfolio-Dev",
-      originalUrl: "https://devsite.portfolio.com.br/devname-123456",
-      accessCount: 30,
-    },
-    {
-      id: "2",
-      shortUrl: "Rocketseat",
-      originalUrl: "https://rocketseat.com.br",
-      accessCount: 10,
-    },
-  ],
+  links: [],
   addLink: (link) => set((state) => ({ links: [link, ...state.links] })),
   setLinks: (links) => set({ links }),
 }));
