34e2089e02d2a04e6f1cf566048898dbe9e16ec2
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2026-02-14 22:46:40 -0300

    fix: rollback link-access.repository

--- server/src/repository/link-access.repository.ts
+++ server/src/repository/link-access.repository.ts
@@ -1,39 +1,23 @@
 import { prisma } from '../lib/prisma';
 
 const create = async (linkId: string) => {
-
-
-  // const [, updatedLink] = await prisma.$transaction([
-  //   prisma.linkAccess.create({
-  //     data: {
-  //       linkId,
-  //     },
-  //   }),
-  //   prisma.link.update({
-  //     where: {
-  //       id: linkId,
-  //     },
-  //     data: {
-  //       accessCount: {
-  //         increment: 1,
-  //       },
-  //     },
-  //   }),
-  // ]);
-
-  const x = await prisma.linkAccess.upsert({
-    where: {linkId},
-    create: {
-      linkId,
-    },
-    update: {
-      accessedAt: {
-        increment: 1,
-      }
-    }
-
-    
-  })
+  const [, updatedLink] = await prisma.$transaction([
+    prisma.linkAccess.create({
+      data: {
+        linkId,
+      },
+    }),
+    prisma.link.update({
+      where: {
+        id: linkId,
+      },
+      data: {
+        accessCount: {
+          increment: 1,
+        },
+      },
+    }),
+  ]);
 
   return updatedLink;
 };
